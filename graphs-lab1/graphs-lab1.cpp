3724a9afd66724f2839abcdb918c306494de4ab7
MonkaS <danchik646@gmail.com>
2024-03-01 14:50:48 +0300

    start + data

--- graphs-lab1/graphs-lab1.cpp
+++ graphs-lab1/graphs-lab1.cpp
@@ -1,8 +1,33 @@
 ﻿// Вариант 19
+// Дано бинарное дерево. Найти все поддеревья, структура которых совпадает с заданной.
 
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct Node {
+    int value;
+    Node* right;
+    Node* left;
+};
+
+struct BinaryTree {
+    Node* root;
+
+    void insert_node(int value, Node* node) {
+        if (root == NULL) {
+            root = new Node();
+            return;
+        }
+
+
+    }
+};
 
 int main()
 {
+    ifstream fs("string.txt", ios::in | ios::binary);
+    fs.close();
     std::cout << "Hello World!\n";
 }
